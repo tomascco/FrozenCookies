da86045099b895e1dc25850c2a07a37ad4e87b75
MakingBaconPancakes <48418858+MakingBaconPancakes@users.noreply.github.com>
2019-05-11 17:56:56 -0500

    Update frozen_cookies.js

--- frozen_cookies.js
+++ frozen_cookies.js
@@ -4,7 +4,7 @@ var scriptElement = document.getElementById('frozenCookieScript') !== null ?
     document.getElementById('modscript_frozen_cookies');
 var baseUrl = scriptElement !== null ?
     scriptElement.getAttribute('src').replace(/\/frozen_cookies\.js$/, '') :
-    'https://mtarnuhal.github.io/FrozenCookies/';
+    'https://MakingBaconPancakes.github.io/FrozenCookies/';
 var FrozenCookies = {
     'baseUrl': baseUrl,
     'branch': '',
