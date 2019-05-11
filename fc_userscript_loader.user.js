b527a3de64b869d7729cbe2313a6225f755884c5
MakingBaconPancakes <48418858+MakingBaconPancakes@users.noreply.github.com>
2019-05-11 17:56:27 -0500

    Update fc_userscript_loader.user.js

--- fc_userscript_loader.user.js
+++ fc_userscript_loader.user.js
@@ -3,19 +3,19 @@
 // @version        github-latest
 // @description    Userscript to load Frozen Cookies written by Icehawk78, forked by Lordshinjo
 // @author         Lordshinjo
-// @homepage       https://github.com/Mtarnuhal/FrozenCookies
+// @homepage       https://github.com/MakingBaconPancakes/FrozenCookies
 // @include        http://orteil.dashnet.org/cookieclicker/
-// @updateURL      https://mtarnuhal.github.io/FrozenCookies/fc_userscript_loader.user.js
-// @downloadURL    https://mtarnuhal.github.io/FrozenCookies/fc_userscript_loader.user.js
+// @updateURL      https://MakingBaconPancakes.github.io/FrozenCookies/fc_userscript_loader.user.js
+// @downloadURL    https://MakingBaconPancakes.github.io/FrozenCookies/fc_userscript_loader.user.js
 // @run-at         document-start
 // ==/UserScript==
 
-// Dev:       https://github.com/Mtarnuhal/FrozenCookies/development/
-// Master:    https://github.com/Mtarnuhal/FrozenCookies/master/
+// Dev:       https://github.com/MakingBaconPancakes/FrozenCookies/development/
+// Master:    https://github.com/MakingBaconPancakes/FrozenCookies/master/
 // Github.io: http://lordshinjo.github.io/FrozenCookies/
 
 function LoadFrozenCookies() {
-    Game.LoadMod('https://mtarnuhal.github.io/FrozenCookies/frozen_cookies.js');
+    Game.LoadMod('https://MakingBaconPancakes.github.io/FrozenCookies/frozen_cookies.js');
 }
 
 window.addEventListener("load", LoadFrozenCookies, false);
