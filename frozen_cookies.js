1915cab8c755bee22d7412761ae161443e015f64
MakingBaconPancakes <48418858+MakingBaconPancakes@users.noreply.github.com>
2019-05-11 19:57:22 -0500

    Undid reroll

--- frozen_cookies.js
+++ frozen_cookies.js
@@ -30,7 +30,7 @@ var script_list = [
     FrozenCookies.baseUrl + '/fc_main.js',
     FrozenCookies.baseUrl + '/fc_button.js',
     FrozenCookies.baseUrl + '/fc_spellpredict.js'
-    FrozenCookies.baseUrl + '/fc_spellreroll.js'
+// wouldn't work    FrozenCookies.baseUrl + '/fc_spellreroll.js'
 ]
 
 FrozenCookies.loadInterval = setInterval(function() {
